ff7be33c44ecdfc0207416fc1527cfa4bfb17045
Yasmine Brooks <yasmine.olivia.brooks@live.mercer.edu>
2019-12-04 11:46:44 -0500

    adding calendar files to test

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import events from './components/events'
+import { render } from 'react-dom';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import './styles.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+class App extends Component {
+  constructor() {
+    super();
+    //const now = new Date();
+    //const events=myevents;
+    this.state = {
+        name: 'React',
+        events
+      };
+      
+  }
+  
+
+  render() {
+    return (
+      <div>
+        <div style={{ height: '500pt'}}>
+          <Calendar
+          //function to change the styles of the events
+            events = {this.state.events}
+                        eventPropGetter={event => { 
+                let newStyle = {
+                    background: event.squad,
+                    color: 'black',
+                    borderRadius: "5px",
+                    border: "none" 
+                };
+                return { style:  newStyle  }; 
+            }
+            } 
+            startAccessor="start"
+            endAccessor="end"
+            defaultDate={moment().toDate()}
+            localizer={localizer}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
